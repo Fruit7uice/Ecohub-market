df79636c0c4a7407b1110788894d4dfcb4e0c740
Frogblast <89352653+Frogblast@users.noreply.github.com>
2023-09-11 17:05:03 +0200

    Folder creation and server connection

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
-
-app.get('/', (req, res) => {
-    res.sendFile('./public/index.html')
-})
+app.use(express.static('public'));
 
 app.listen(port)
 

0feaf55ad360094f14a19a0498c7fe3fc23f1f0d
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2023-09-11 17:06:01 +0200

    Stand alone functioning map-API
    Regarding USERSTORY 4 we have managed to gain knowledge on how to implement the map API and we have also managed to create a map where we can plot pins and store information on location.
    
    Co-Authored-By: Alva Johansson <83019444+AlvaJohansson@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function initMap() {
+
+/*function initMap() {
     // Create a map centered at a specific location
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: YOUR_LATITUDE, lng: YOUR_LONGITUDE },
+        center: { lat: 57.705571, lng: 11.973321 },
         zoom: 12 // Set the initial zoom level
     });
 
@@ -30,5 +31,54 @@ function initMap() {
         marker.addListener("click", () => {
             infoWindow.open(map, marker);
         });
+    });
+
+
+
+}*/
+
+
+
+
+/**
+ * @initMap
+ * Firstly the map is being centered to #center-coordinates of ones choice
+ * Zooms to a certain level of zoom
+ * 
+ * Markers can be predefined, with the help of our database of lat & long values
+ * markers are looped through and added to the map
+ * 
+**/
+
+
+let map; // Define a global variable to hold the map object
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 57.70544078751393, lng: 11.985471236320263 },
+        zoom: 12
+    });
+
+    // Add a right-click event listener to the map
+    map.addListener('rightclick', function(event) {
+        addMarker(event.latLng); // Call the addMarker function with the clicked coordinates
+    });
+}
+
+function addMarker(location) {
+    // Create a marker
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+    });
+
+    // Create an info window
+    const infoWindow = new google.maps.InfoWindow({
+        content: `Latit kebab  ude: ${location.lat()}, Longitude: ${location.lng()}`
+    });
+
+    // Add a click event listener to the marker
+    marker.addListener("click", () => {
+        infoWindow.open(map, marker);
     });
 }

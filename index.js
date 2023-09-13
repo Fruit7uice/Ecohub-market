add81b8e46f91b5d745c4c105092fcc1db303c11
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2023-09-13 10:27:20 +0200

    Added list functionality
    The map is now able to directly display the pre-defined pins.
    
    Co-Authored-By: Alva Johansson <83019444+AlvaJohansson@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,45 +1,4 @@
 
-/*function initMap() {
-    // Create a map centered at a specific location
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 57.705571, lng: 11.973321 },
-        zoom: 12 // Set the initial zoom level
-    });
-
-    // Define an array of markers with their respective coordinates and information
-    const markers = [
-        {
-            position: { lat: 40.7128, lng: -74.0060 }, // Example coordinates (New York City)
-            info: "Marker 1 - Latitude: 40.7128, Longitude: -74.0060"
-        },
-        // Add more markers here as needed
-    ];
-
-    // Loop through the markers and create them on the map
-    markers.forEach(markerInfo => {
-        const marker = new google.maps.Marker({
-            position: markerInfo.position,
-            map: map,
-        });
-
-        // Create an info window to display when the marker is clicked
-        const infoWindow = new google.maps.InfoWindow({
-            content: markerInfo.info
-        });
-
-        // Add a click event listener to the marker
-        marker.addListener("click", () => {
-            infoWindow.open(map, marker);
-        });
-    });
-
-
-
-}*/
-
-
-
-
 /**
  * @initMap
  * Firstly the map is being centered to #center-coordinates of ones choice
@@ -58,6 +17,38 @@ function initMap() {
         center: { lat: 57.70544078751393, lng: 11.985471236320263 },
         zoom: 12
     });
+    const name = "name1";
+    const name2 = "name2"
+    const names = ["hej", "då"];
+    // Define an array of markers with their respective coordinates and information
+    const markers = [
+        {
+            name: name,
+            position: { lat: 57.71, lng: 11.9060 }, 
+            info: names[0] + " - latitude: 57.71, Longitude: 11.1060"
+        },
+        {
+            position: { lat: 57.7128, lng: 11.8060 }, 
+            info: names[1] + " - Latitude: 57.7128, Longitude: 11.0060"
+        }
+        // Add more markers here as needed
+    ];
+
+    // Loop through the markers and create them on the map
+    markers.forEach(markerInfo => {
+        const marker = new google.maps.Marker({
+            position: markerInfo.position,
+            map: map,
+        });
+
+        // Add a click event listener to the marker
+        marker.addListener("click", () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: markerInfo.info
+            });
+            infoWindow.open(map, marker);
+        });
+    });
 
     // Add a right-click event listener to the map
     map.addListener('rightclick', function(event) {
@@ -73,8 +64,9 @@ function addMarker(location) {
     });
 
     // Create an info window
+    
     const infoWindow = new google.maps.InfoWindow({
-        content: `Latit kebab  ude: ${location.lat()}, Longitude: ${location.lng()}`
+        content: `Latitude: ${location.lat()}, Longitude: ${location.lng()}`
     });
 
     // Add a click event listener to the marker

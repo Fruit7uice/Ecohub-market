3871bd58a4f8da5cea142af3c27cc20fa197e1c2
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2023-09-19 16:00:21 +0200

    html fetch to query for all Sellers

---
+++ connection.js
@@ -0,0 +1,44 @@
+module.exports = {
+    retrieveAllSellers,
+    getClient
+};
+
+const { Client } = require('pg');
+
+
+
+// Creates a client for a local database with default attributes.
+// CHANGE to become more modular, with parameters. 
+function getClient(){
+    const client = new Client({
+        user: 'postgres',
+        host: '127.0.0.1', // e.g., your_rds_endpoint.amazonaws.com
+        database: 'testdb',
+        password: 'postgres',
+        port: 5432, // Default PostgreSQL port
+    });
+    return client;
+}
+
+// Create a client instance
+function retrieveAllSellers(client) {
+    return new Promise((resolve, reject) => {
+        client.connect()
+            .then(() => console.log('Connected to PostgreSQL database'))
+            .catch(err => reject('Error connecting to PostgreSQL', err));
+
+        client.query('SELECT * FROM Sellers;', (err, res) => {
+            if (err) {
+                reject('Error executing query', err);
+            } else {
+                console.log("Rows Retrieved")
+                resolve(res.rows);
+            }
+            client.end(); // Close the connection after the query
+            console.log("Client terminated!")
+        });
+    });
+}
+
+
+

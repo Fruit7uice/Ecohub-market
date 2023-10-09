68a870070ffeac98d6d944e9b530867ffe275d7e
Frogblast <89352653+Frogblast@users.noreply.github.com>
2023-10-09 19:09:50 +0200

    All english words in productCol.js

--- public/productCol.js
+++ public/productCol.js
@@ -45,7 +45,6 @@ function populateListOfProducts(jsonList){
             img.src = './assets/' + product.category.replace(/\s/g, '') + '.jpg';
             img.className = "product-img";
 
-
             const timestamp = document.createElement('p');
             timestamp.textContent = product.timeofupload.split('T')[0];
             timestamp.id = 'timestamp';
@@ -70,29 +69,20 @@ function populateListOfProducts(jsonList){
         addAllMarkersToMap();
 }
 
-
-
 //Function to dislay the marker when it gets clicked in the sellers column
 function displayPin(index) {
     const item = savedJson[index];
     addMarker(item.coordinates, item.locationadress, item.name)
-
-
 }
 
 
 //Function to add all markers to map from beginning
 function addAllMarkersToMap() {
-
     for (let i = 0; i < savedJson.length; i++) {
         displayPin(i)
         console.log("whole object = ", savedJson[i])
     }
-
 }
-
-
-
 
 
 function populateInfoBox(index){
@@ -116,19 +106,15 @@ function populateInfoBox(index){
         const priceUnitType = document.createElement('h2');
         priceUnitType.innerHTML = item.name + ": " + item.price + " kr "+ " / " + item.unit;
 
-        // const unit = document.createElement('h2');
-        // unit.innerHTML = item.unit;
-
     titlePrice.appendChild(title);
     titlePrice.appendChild(priceUnitType);
-    // titlePrice.appendChild(unit);
     //***********
 
     // *** populate description
 
     const descriptionHeader = document.createElement('h4');
     const description = document.createElement('p');
-    descriptionHeader.innerText = "Beskrivning";
+    descriptionHeader.innerText = "Description";
     description.innerText = item.description;
     descriptionContainer.appendChild(descriptionHeader);
     descriptionContainer.appendChild(description)
@@ -138,16 +124,14 @@ function populateInfoBox(index){
     // *** populate image
     const image = document.createElement('img'); 
     image.src = './assets/' + item.category.replace(/\s/g, '') + '.jpg';
-    // imageContainer.innerHTML = "IMAGE";
     imageContainer.appendChild(image);
-
     //********
     
 
     // *** populate location
     const locationHeader = document.createElement('h4');
     const location = document.createElement('p');
-    locationHeader.innerHTML = "Säljes vid";
+    locationHeader.innerHTML = "Sells at";
     location.innerHTML = item.locationadress;
     locationContainer.appendChild(locationHeader);
     locationContainer.appendChild(location);
@@ -156,10 +140,8 @@ function populateInfoBox(index){
     // *** populate seller
     const seller = document.createElement('h4');
     const sellerDescription = document.createElement("p");
-    seller.innerHTML = "Säljes av"; //Hårdkodat för nu tills vi har ett attribut för seller description i databasen.
-    sellerDescription.innerHTML = "Krösus Sork är en affärsman och har en hel stab av arbetande sorkar i olika positioner.";
-    // seller.innerHTML = item.seller;
-    // sellerDescription.innerHTML = item.sellerDescription;
+    seller.innerHTML = "Vendor";
+    sellerDescription.innerHTML = "Vendor description here"; // TODO: lägg in description för säljaren här
     sellerContainer.appendChild(seller);
     sellerContainer.appendChild(sellerDescription);
     //********

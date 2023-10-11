94e49eeaa014162b22a5711dff6f67dc80019f20
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2023-10-11 14:36:48 +0200

    Modular fix to saving the formInfo

--- public/thirdForm.js
+++ public/thirdForm.js
@@ -3,18 +3,43 @@
 
 // This function saves the value of the input field with the id 'firstName' into the local storage.
 function saveInfo() {
-    var name = document.getElementById('firstName').value; // Get the value of the input field
-    localStorage.setItem('welcome-name', name); // Store the value in local storage with key 'welcome-name'
-    displayWelcomeMessage(); // Call the function to display the welcome message
+    // var firstName = document.getElementById('firstName').value; // Get the value of the input field
+    // var lastName = document.getElementById('lastName').value; // Get the value of the input field
+    // var phoneNumber = document.getElementById('phoneNumber').value; // Get the value of the input field
+    // var personalNumber = document.getElementById('personalNumber').value; // Get the value of the input field
+    // var adress = document.getElementById('adress').value; // Get the value of the input field
+    // var city = document.getElementById('city').value; // Get the value of the input field
+    // var zipCode = document.getElementById('zipCode').value; // Get the value of the input field
+    // var productName = document.getElementById('productName').value; // Get the value of the input field
+    // var productDescription = document.getElementById('productDescription').value; // Get the value of the input field
+    // localStorage.setItem('welcome-name', name); // Store the value in local storage with key 'welcome-name'
+    // displayWelcomeMessage(); // Call the function to display the welcome message
+
+    var sellerForm = document.getElementById("form-1");
+    var inputs = sellerForm.getElementsByTagName('input');
+    for(let i = 0; i< inputs.length; i++){
+        let fInput = inputs[i];
+        localStorage.setItem(fInput.name, fInput.value);
+    } 
+    displayWelcomeMessage();
 }
 
 // This function displays the welcome message using the value stored in local storage.
 function displayWelcomeMessage() {
-    var name = localStorage.getItem('welcome-name'); // Retrieve the value from local storage
-    if (name) { // Check if a name was retrieved
-        document.getElementById('welcome-name').textContent = name; // Set the content of the element with id 'welcome-name'
-        console.log(name) // Print the name to the console
-    }
+    var savedFirstName = localStorage.getItem('firstName'); // Retrieve the value from local storage
+    // var lastName = localStorage.getItem('lastName'); // Retrieve the value from local storage
+    // var phoneNumber = localStorage.getItem('wname'); // Retrieve the value from local storage
+    // var personalNumber = localStorage.getItem('welcome-name'); // Retrieve the value from local storage
+    // var name = localStorage.getItem('welcome-name'); // Retrieve the value from local storage
+    // var name = localStorage.getItem('welcome-name'); // Retrieve the value from local storage
+    // var name = localStorage.getItem('welcome-name'); // Retrieve the value from local storage
+    // var name = localStorage.getItem('welcome-name'); // Retrieve the value from local storage
+    // var name = localStorage.getItem('welcome-name'); // Retrieve the value from local storage
+    // if (name) { // Check if a name was retrieved
+        const s = document.getElementById('savedFirstName'); // Set the content of the element with id 'welcome-name'
+        s.innerText = savedFirstName;
+        console.log(savedFirstName) // Print the name to the console
+    
 }
 
 
